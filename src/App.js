1e887dfeaa2b8fea82a1612d1c529b24c5dd51db
RvInfo Sistemas <ricardovieirasp@outlook.com>
2022-02-14 22:33:08 -0300

    curso 1 concluido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
+import FormularioCadastro from "./components/FormularioCadastro";
+import ListaDeNotas from "./components/ListaDeNotas";
+import "./assets/App.css";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super();
+    this.notas = [];
+    this.state = {
+      notas:[]
+    };
+  }
+
+  criarNota(titulo, texto) {
+    const novaNota = {titulo,texto};
+    const novoArrayNotas = [...this.state.notas, novaNota];
+
+    const novoEstado = {
+      notas:novoArrayNotas
+    };
+
+    this.setState(novoEstado);
+  }
+
+  render(){
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
+      </section>
+    );  
+  }
 }
 
 export default App;
